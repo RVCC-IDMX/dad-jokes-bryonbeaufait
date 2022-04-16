85bce82654511bf6e099c2eefcb0187ebef37c52
Bryon <beaufaitb@gmail.com>
2022-04-16 13:56:05 -0400

    finish dad jokes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,12 @@
 const getRadomDadJoke = async () => {
-  const url = 'https://icanhazdadjoke.com/';
-  const jokeStream = await fetch(url, {
-    headers: {
-      Accept: 'application/json',
-    },
-  });
+  // const url = 'https://icanhazdadjoke.com/';
+  // const jokeStream = await fetch(url, {
+  //   headers: {
+  //     Accept: 'application/json',
+  //   },
+  // });
+  const url = '/.netlify/functions/jokes';
+  const jokeStream = await fetch(url);
   const jsonJoke = await jokeStream.json();
   const { joke } = jsonJoke;
   return joke;
